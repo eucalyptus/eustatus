c0fe8e7394bea9c8c0b3169667ba7442a6f4f3dd
Michael De La Rue (Nokia Siemens Networks sp z o.o.) <git-krviq.nsn-mddelarue@spamgourmet.com>
2013-02-05 00:07:11 +0200

    Add licensing information

--- readinsertimages.py
+++ readinsertimages.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 #
-#  Python script to read tidied and stripped xml from euca-describe-images --debug to postgres DB as an user level user
+#  Python script to read tidied and stripped xml from
+#  euca-describe-images --debug to postgres DB as an user level user
 #
+#  Copyright 2013 Nokia Siemens Networks, Authored by Teemu Jalonen
 #
-#  Copyright Nokia Siemens Networks 2013, Authored by Teemu Jalonen
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 #
 
 import sys
--- readinsertinstances.py
+++ readinsertinstances.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python
 #
-#  Python script to read tidied and stripped xml from euca-describe-images --debug to postgres DB as an hist user
+#  Python script to read tidied and stripped xml from
+#  euca-describe-images --debug to postgres DB as an hist user
 #
+#  Copyright 2013 Nokia Siemens Networks, Authored by Teemu Jalonen
 #
-#  Copyright Nokia Siemens Networks 2013, Authored by Teemu Jalonen
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #
 
-import os
-import sys
-import string
-import psycopg2
-import datetime
-import StringIO
-from xml.etree.ElementTree import iterparse
-import argparse
 
 sampledatetime = datetime.datetime.now()
 #print "sampledatetime: ",sampledatetime
