26dc092192ce403a21bb77089ef18d002f2e9966
teejalon <teemu.jalonen@iki.fi>
2013-11-12 15:21:22 +0200

    moving files from teejalon/cloudhistory i#	new file:   create_accounts_table.sql

---
+++ cloudhistory/files/create_accounts_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "accounthistory"
+(
+sampledatetime timestamp without time zone,
+AccountId varchar(255) NOT NULL,
+AccountName varchar(255),
+AccountEmail varchar(255),
+CONSTRAINT "accounthistory_pkey" PRIMARY KEY (AccountId )
+);
