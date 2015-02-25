f7438dcfe92dc9578fdf22d585a5c81d274238f7
teejalon <teemu.jalonen@iki.fi>
2015-02-25 09:56:14 +0200

    mem removal

--- cloudhistory/files/create_instance_load_table.sql
+++ cloudhistory/files/create_instance_load_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "instanceloadhistory"
 (
 sampledatetime timestamp without time zone NOT NULL,
-instanceid	varchar(255) NOT NULL,
-cpuloadpercent		double precision,
-iopsload		double precision,
-nettxbytes		double precision,
-netrxbytes		double precision,
-mem			double precision,
+instanceid varchar(255) NOT NULL,
+cpuloadpercent double precision,
+readrequests double precision,
+writerequests double precision,
+netrxbytes double precision,
+nettxbytes double precision,
 CONSTRAINT "instanceloadhistory_pkey" PRIMARY KEY (sampledatetime,instanceid )
 );
